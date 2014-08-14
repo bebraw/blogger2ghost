58d805ef142658a7bf391e4aba53b9661f19fdbe
James Homer <jameshomer85@gmail.com>
2014-08-15 00:57:02 +0100

    download images option - see readme for details

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 #!/usr/bin/env node
+
 var program = require('commander');
 
 var convert = require('./lib/convert');
 var loadJSON = require('./lib/load_json');
+var download = require('./lib/download');
 
 var VERSION = require('./package.json').version;
-
 
 main();
 
 function main() {
-    program.version(VERSION).
-        option('-i --input <input file>').
-        option('-a --authors <authors file>').
-        parse(process.argv);
+    program.version(VERSION)
+        .option('-i --input <input file>')
+        .option('-a --authors <authors file>')
+        .option('-d --download <download location>')
+        .parse(process.argv);
 
-    if(!program.input) {
+    if (!program.input) {
         return console.error('Missing input file! Pass it using -i parameter');
     }
 
     loadJSON(program.input, function(err, json) {
-        if(err) {
+        if (err) {
             return console.error('Failed to load input JSON');
         }
 
-        loadJSON(program.authors, function(err, authors) {
-            if(err) {
-                console.warn('Authors JSON not specified or invlaid');
-            }
+        if (program.authors) {
+            loadJSON(program.authors, function(err, authors) {
+                if (err) {
+                    return console.warn('Failed to load authors JSON');
+                }
 
-            json.authors = authors || {};
+                runTask(json, program, authors);
+            });
 
-            console.log(JSON.stringify(convert(json)));
-        });
+        } else {
+            runTask(json, program);
+        }
     });
 }
 
+function runTask(json, program, authors) {
+    var result = convert(json, {
+        authors: authors || {},
+        download: program.download
+    });
+
+    if (result.images && program.download) {
+        console.warn('Downloading ' + result.images.length + ' images, this may take some time');
+
+        download(program.download, result.images, function() {
+            delete result.images;
+
+            console.log(JSON.stringify(result));
+        });
+
+    } else {                
+        console.log(JSON.stringify(result));
+    }    
+}
