6a9e297ca6015c4ef36bdb7146637b5f0e08ad0b
Juho Vepsalainen <bebraw@gmail.com>
2015-01-06 10:21:17 +0200

    Fix tag conversion specification

--- tests/test_convert_tag.js
+++ tests/test_convert_tag.js
@@ -3,12 +3,12 @@ var suite = require('suite.js');
 var convertTag = require('../lib/convert_tag');
 
 suite(convertTag, [
-    'foo', 'foo',
-    'self help', 'self_help',
-    'self-help', 'self__help',
-    'another self help', 'another_self_help',
-    'another-self-help', 'another__self__help',
-    'SciFi', 'sci_fi',
-    'SciFiToo', 'sci_fi_too',
-    'sci-fi', 'sci__fi'
+    'foo', 'Foo',
+    'self help', 'Self Help',
+    'self-help', 'Self-help',
+    'another self help', 'Another Self Help',
+    'another-self-help', 'Another-self-help',
+    'SciFi', 'Scifi',
+    'SciFiToo', 'Scifitoo',
+    'sci-fi', 'Sci-fi'
 ]);
